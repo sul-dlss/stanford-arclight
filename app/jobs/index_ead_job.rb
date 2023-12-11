d020bbbfbcfcb254d3c10e99253379efe6b21b4e
Cory Lown <corylown@stanford.edu>
2023-12-11 16:22:55 -0500

    Let EadIndexJob infer the repository code if it's not provided as an argument

--- app/jobs/index_ead_job.rb
+++ app/jobs/index_ead_job.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'open3'
+require 'pathname'
 
 ##
 # Background job to index an EAD XML file in Solr
@@ -8,10 +9,10 @@ class IndexEadJob < ApplicationJob
   class IndexEadError < StandardError; end
 
   def perform(file_path:,
-              repo_code:,
+              arclight_repo_code: nil,
               solr_url: ENV.fetch('SOLR_URL', Blacklight.default_index.connection.base_uri),
               app_dir: Rails.root)
-    env = { 'REPOSITORY_ID' => repo_code }
+    env = { 'REPOSITORY_ID' => arclight_repo_code || arclight_repository_code(file_path) }
     cmd = "bundle exec traject -u #{solr_url} -i xml -c ./lib/traject/sul_config.rb #{file_path}"
 
     output, status = Open3.capture2(env, cmd, chdir: app_dir)
@@ -19,5 +20,13 @@ class IndexEadJob < ApplicationJob
     raise IndexEadError, output unless status.success?
 
     Rails.logger.debug output
+  end
+
+  private
+
+  # Infer the arclight repository code from the file_path as
+  # the name of the immediate parent directory of the file
+  def arclight_repository_code(file_path)
+    Pathname.new(file_path).parent.split.last.to_s
   end
 end
--- spec/jobs/index_ead_job_spec.rb
+++ spec/jobs/index_ead_job_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe IndexEadJob do
 
   it 'sends a command to index the EAD in Solr with traject' do
     described_class.perform_now(file_path: 'data/sample.xml',
-                                repo_code: 'ars',
+                                arclight_repo_code: 'ars',
                                 solr_url: 'http://localhost/solr/core',
                                 app_dir: '/some/directory')
     expect(Open3).to have_received(:capture2).with(
@@ -31,6 +31,16 @@ RSpec.describe IndexEadJob do
     )
   end
 
+  context 'when the arclight repository code is not provided as an argument' do
+    it 'infers the repository code from the file path' do
+      described_class.perform_now(file_path: '/data/archive/sample.xml',
+                                  solr_url: 'http://localhost/solr/core',
+                                  app_dir: '/some/directory')
+
+      expect(Open3).to have_received(:capture2).with({ 'REPOSITORY_ID' => 'archive' }, anything, anything)
+    end
+  end
+
   context 'when the traject command does not succeed' do
     before do
       allow(status).to receive(:success?).and_return(false)
@@ -39,7 +49,7 @@ RSpec.describe IndexEadJob do
     it 'raises an error' do
       expect do
         described_class.perform_now(file_path: 'data/sample.xml',
-                                    repo_code: 'ars',
+                                    arclight_repo_code: 'ars',
                                     solr_url: 'http://localhost/solr/core',
                                     app_dir: '/some/directory')
       end.to raise_error(IndexEadJob::IndexEadError)
