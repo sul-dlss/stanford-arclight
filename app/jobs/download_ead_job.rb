71667e04f62df899ee1136c9d70f8aee9550b7b5
Cory Lown <corylown@stanford.edu>
2023-12-11 16:22:25 -0500

    Refactor so that DownloadEadJob doesn't have to care about repo codes

--- app/jobs/download_ead_job.rb
+++ app/jobs/download_ead_job.rb
@@ -3,20 +3,25 @@
 ##
 # Background job to download an EAD XML file from ArchiveSpace
 class DownloadEadJob < ApplicationJob
-  def perform(ead_id:, resource_uri:, repo_code:, data_dir: ENV.fetch('DATA_DIR', 'data'), index: false)
+  # @example DownloadEadJob.perform_later(resource_uri: '/repositories/2/resources/5363',
+  #                                       file_name: 'ars0018')
+  # @param resource_uri [String] the URI of the resource in ArchivesSpace, such as '/repositories/2/resources/5363'
+  # @param file_name [String] the name of the file to be created, such as 'ead1234'
+  # @param data_dir [String] the path where the file should be saved, such as '/opt/app/arclight/data'
+  # @param index [Boolean] if true an IndexEadJob will be queued up with the stored file
+  def perform(resource_uri:, file_name:, data_dir:, index: false)
     client = AspaceClient.new
+
     ead_xml = client.resource_description(resource_uri)
 
-    file_name = ead_id.sub(/(\.xml)?$/i, '.xml')
-    directory = "#{data_dir}/#{repo_code}"
-
-    file_path = File.join(directory, file_name.to_s)
+    xml_file_name = file_name.sub(/(\.xml)?$/i, '.xml')
+    file_path = File.join(data_dir, xml_file_name.to_s)
 
     File.open(file_path, 'wb') do |f|
       ead = Nokogiri::XML(ead_xml, &:noblanks)
       f.puts ead.to_xml(indent: 2)
     end
 
-    IndexEadJob.perform_later(file_path:, repo_code:) if index
+    IndexEadJob.perform_later(file_path:) if index
   end
 end
--- spec/jobs/download_ead_job_spec.rb
+++ spec/jobs/download_ead_job_spec.rb
@@ -12,13 +12,13 @@ RSpec.describe DownloadEadJob do
   end
 
   it 'fetches an EAD from the client service' do
-    described_class.perform_now(ead_id: 'abc123', resource_uri: 'resource/abc123', repo_code: 'archive')
+    described_class.perform_now(resource_uri: 'resource/abc123', file_name: 'abc123', data_dir: '/data/archive/')
 
     expect(client).to have_received(:resource_description).with('resource/abc123')
   end
 
   it 'writes the formatted EAD XML to a file' do
-    described_class.perform_now(ead_id: 'abc123', resource_uri: 'resource/abc123', repo_code: 'archive')
+    described_class.perform_now(resource_uri: 'resource/abc123', file_name: 'abc123', data_dir: '/data/archive/')
 
     expect(file).to have_received(:puts).with("<?xml version=\"1.0\"?>\n<a/>\n")
   end
@@ -26,9 +26,9 @@ RSpec.describe DownloadEadJob do
   context 'when index param value is true' do
     it 'enqueues an indexing job with the EAD file' do
       expect do
-        described_class.perform_now(ead_id: 'abc123', resource_uri: 'resource/abc123', repo_code: 'archive',
+        described_class.perform_now(resource_uri: 'resource/abc123', file_name: 'abc123', data_dir: '/data/archive/',
                                     index: true)
-      end.to enqueue_job(IndexEadJob).once.with(file_path: 'data/archive/abc123.xml', repo_code: 'archive')
+      end.to enqueue_job(IndexEadJob).once.with(file_path: '/data/archive/abc123.xml')
     end
   end
 end
