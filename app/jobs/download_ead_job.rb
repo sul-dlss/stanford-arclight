a7ddd91512265d0a604cf6a9132aced78f0e9931
Cory Lown <corylown@stanford.edu>
2023-12-11 16:49:32 -0500

    Add comment that data_dir must already exist

--- app/jobs/download_ead_job.rb
+++ app/jobs/download_ead_job.rb
@@ -8,6 +8,7 @@ class DownloadEadJob < ApplicationJob
   # @param resource_uri [String] the URI of the resource in ArchivesSpace, such as '/repositories/2/resources/5363'
   # @param file_name [String] the name of the file to be created, such as 'ead1234'
   # @param data_dir [String] the path where the file should be saved, such as '/opt/app/arclight/data'
+  #        the directory specified must already exist
   # @param index [Boolean] if true an IndexEadJob will be queued up with the stored file
   def perform(resource_uri:, file_name:, data_dir:, index: false)
     client = AspaceClient.new
