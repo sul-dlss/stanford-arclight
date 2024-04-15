bea7de82d68afd028628dacb2a4c6910c05b26c2
Cory Lown <corylown@stanford.edu>
2024-04-15 10:38:26 -0400

    Add and configure blacklight_range_limit gem

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -2,6 +2,7 @@
 
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
+  include BlacklightRangeLimit::RangeLimitBuilder
   include Arclight::SearchBehavior
 
   ##
