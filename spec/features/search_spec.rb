b4ef754a6c1d058b305c437221c7334fa94de336
Steve Taylor <sctaylor@stanford.edu>
2024-05-02 11:07:24 -0700

    Change default search grouping to 'collection'

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -2,25 +2,47 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Search', type: :feature do
-  before do
-    visit search_catalog_path
+RSpec.describe 'Searching', :js, type: :feature do
+  context 'when not within a collection' do
+    before do
+      visit search_catalog_path
 
-    # Fill in the search input with some text
-    fill_in 'q', with: 'Example search query'
+      # Fill in the search input with some text
+      fill_in 'q', with: 'Example search query'
 
-    click_button 'Search'
+      click_button 'Search'
+    end
+
+    it 'renders the search results page with default params' do
+      expect(page.current_url).to include('search_field=keyword&q=Example+search+query')
+      expect(page.current_url).to include('group=true')
+
+      expect(page).to have_content(/You searched for:\s*Keyword Example search query/)
+    end
+
+    it 'creates Start Over link with the group=true parameter' do
+      click_link('Start Over')
+
+      expect(page).to have_current_path(search_catalog_path(group: true))
+    end
   end
 
-  it 'Renders the search results page with default params' do
-    expect(page).to have_current_path('/catalog?search_field=keyword&q=Example+search+query')
-
-    expect(page).to have_content('You searched for: Keyword Example search query')
+  context 'when within a collection' do
+    it 'does not group by collection' do
+      visit solr_document_path(id: 'ARS-0043')
+      fill_in 'q', with: 'jazz'
+      click_on 'search'
+      expect(page.current_url).not_to include('group=true')
+    end
   end
 
-  it 'Creates Start Over link with the group=true parameter' do
-    click_link('Start Over')
-
-    expect(page).to have_current_path(search_catalog_path(group: true))
+  context 'when within a collection and user selects group by all collections' do
+    it 'groups by collection' do
+      visit solr_document_path(id: 'ARS-0043')
+      fill_in 'q', with: 'jazz'
+      select('all collections', from: 'within_collection')
+      click_on 'search'
+      expect(page.current_url).to include('group=true')
+    end
   end
 end
