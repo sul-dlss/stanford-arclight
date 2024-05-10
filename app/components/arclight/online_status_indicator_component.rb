d8cd6b99d806aad49cd03ca7100d704f05fd1714
mlongley <mlongley@stanford.edu>
2024-05-10 11:35:54 -0500

    Implement custom digital contents icon, tooltip, and alert

---
+++ app/components/arclight/online_status_indicator_component.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Overrides Arclight's OnlineStatusIndicatorComponent to replace with a custom template that includes our tooltip
+module Arclight
+  # Render an online status indicator for a document
+  class OnlineStatusIndicatorComponent < Blacklight::Component
+    def initialize(document:, **)
+      @document = document
+      super
+    end
+
+    def render?
+      @document.online_content?
+    end
+
+    def icon_with_tooltip
+      icon = <<~HTML
+        <span class="al-online-content-icon" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-html="true" data-bs-title="Includes digital content">
+            #{helpers.blacklight_icon(:online)}
+        </span>
+      HTML
+      icon.html_safe # rubocop:disable Rails/OutputSafety
+    end
+  end
+end
--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -50,7 +50,9 @@ class CatalogController < ApplicationController
 
     config.header_component = Arclight::HeaderComponent
     config.logo_link = 'https://library.stanford.edu'
-    config.add_results_document_tool(:online, component: Arclight::OnlineStatusIndicatorComponent)
+    # add_results_document_tool effectively creates an Blacklight action
+    # Then actions are interated over in search_results_document_component.html.erb
+    # See https://github.com/projectblacklight/blacklight/blob/6c6a72066a6330a815603e839b316958f6321dd7/lib/blacklight/configuration.rb#L560
     config.add_results_document_tool(:arclight_bookmark_control, component: Arclight::BookmarkComponent)
 
     config.add_results_collection_tool(:group_toggle)
