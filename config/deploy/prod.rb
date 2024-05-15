776ac52ad4e671b7286d730633e21daa5a257e3f
Cory Lown <corylown@stanford.edu>
2024-05-15 08:56:33 -0400

    Set up prod deploy for prod-a and b

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-server 'sul-arclight-prod.stanford.edu', user: 'arclight', roles: %w[web db app]
+server 'sul-arclight-prod-a.stanford.edu', user: 'arclight', roles: %w[web db app]
+server 'sul-arclight-prod-b.stanford.edu', user: 'arclight', roles: %w[web app]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
