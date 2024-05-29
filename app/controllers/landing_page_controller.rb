e523dd5db3937521593736e75de2cea7dd2fc78d
Cory Lown <corylown@stanford.edu>
2024-05-29 11:27:14 -0400

    Configure logo link for landing page

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -7,6 +7,7 @@ class LandingPageController < ApplicationController
   configure_blacklight do |config|
     config.header_component = LandingPage::HeaderComponent
     config.user_util_links_component = LandingPage::UserUtilLinksComponent
+    config.logo_link = 'https://library.stanford.edu'
 
     # Configuration for autocomplete suggestor
     config.autocomplete_enabled = true
