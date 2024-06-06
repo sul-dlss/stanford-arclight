18640df70930739424d521236b5aab86a9d4e4d5
Steve Taylor <sctaylor@stanford.edu>
2024-06-06 16:55:00 -0700

    Prevent keyboard focus on hidden feedback form

--- spec/features/feedback_form_spec.rb
+++ spec/features/feedback_form_spec.rb
@@ -3,8 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Feedback Form', :js, type: :feature do
-  it 'is visible after clicking the feedback link' do
+  it 'is visible only after clicking the feedback link' do
     visit root_path
+
+    feedback = page.find(:css, '#feedback', visible: false)
+    expect(feedback.native.style('visibility')).to eq('hidden')
+    expect(page).to have_css('#feedback', visible: :hidden)
+
     click_link 'Feedback'
     within '#feedback' do
       expect(page).to have_css('form')
