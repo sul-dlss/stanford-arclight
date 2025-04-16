a2c25a2573c8015ba0c4f0c9d3927cd9b51bcb37
Cory Lown <corylown@stanford.edu>
2025-04-16 15:10:07 -0400

    Add download bookmarks to CSV

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,6 +5,8 @@ class BookmarksController < CatalogController
   include Blacklight::Bookmarks
 
   configure_blacklight do |config|
+    config.index.respond_to.csv = true
     config.view_config(:index).collection_actions.delete(:group_toggle)
+    config.default_per_page = 50
   end
 end
