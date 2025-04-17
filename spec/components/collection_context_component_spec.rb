ff598df10b5bc0fc6b72ce98ecbdfbc77ae09f58
Cory Lown <corylown@stanford.edu>
2025-04-17 16:33:42 -0400

    Move request button to collection context box

--- spec/components/collection_context_component_spec.rb
+++ spec/components/collection_context_component_spec.rb
@@ -2,11 +2,16 @@
 
 require 'rails_helper'
 
-RSpec.describe CollectionContextComponent, type: :component do
+RSpec.describe CollectionContextComponent, type: :component do # rubocop:disable RSpec/MultipleMemoizedHelpers
   subject(:component) { described_class.new(presenter:, download_component: Arclight::DocumentDownloadComponent) }
 
   let(:presenter) { instance_double(Blacklight::DocumentPresenter, document:) }
   let(:document) { instance_double(SolrDocument, collection:) }
+  let(:repository_config) do
+    instance_double(Arclight::Repository, request_config_present?: true,
+                                          available_request_types: ['aeon_web_ead'],
+                                          request_config_for_type: {})
+  end
   let(:collection) do
     instance_double(SolrDocument, normalized_title: 'Collection Title',
                                   unitid: 'ARS123',
@@ -15,7 +20,11 @@ RSpec.describe CollectionContextComponent, type: :component do
                                   online_item_count: '4',
                                   last_indexed: Time.zone.parse('2025-04-07'),
                                   downloads: downloads,
-                                  collection?: true)
+                                  collection?: true,
+                                  requestable?: true,
+                                  repository_config:,
+                                  ead_file: 'ars123.xml',
+                                  ead_file_without_namespace_href: 'https://example.com/ars123.xml')
   end
   let(:downloads) { instance_double(Arclight::DocumentDownloads, files: [file]) }
   let(:file) { instance_double(Arclight::DocumentDownloads::File, href: 'https://example.com/file.txt', size: 1234, type: 'pdf') }
@@ -34,6 +43,10 @@ RSpec.describe CollectionContextComponent, type: :component do
 
   it 'renders the title' do
     expect(page).to have_text('Collection Title')
+  end
+
+  it 'renders the request link' do
+    expect(page).to have_text('Request')
   end
 
   it 'renders information about the collection' do
