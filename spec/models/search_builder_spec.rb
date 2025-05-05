426ef95446a1bf7a2fb8d05b51edf78416506a4e
Cory Lown <corylown@stanford.edu>
2025-05-05 11:41:14 -0400

    Change mm setting when query contains boolean operators

--- spec/models/search_builder_spec.rb
+++ spec/models/search_builder_spec.rb
@@ -47,4 +47,26 @@ RSpec.describe SearchBuilder do
       end
     end
   end
+
+  describe '#min_match_for_boolean' do
+    subject(:solr_parameters) do
+      search_builder.with(query).processed_parameters
+    end
+
+    context 'when query has a boolean operator' do
+      let(:query) { { q: 'archives AND (recorded OR sound)' } }
+
+      it 'sets min match to 1' do
+        expect(solr_parameters[:mm]).to eq('1')
+      end
+    end
+
+    context 'when query has no boolean operator' do
+      let(:query) { { q: 'archives' } }
+
+      it 'does not set min match' do
+        expect(solr_parameters[:mm]).to be_nil
+      end
+    end
+  end
 end
