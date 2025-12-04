c61fcbf6b5df69d33b45e244bedec47dce4dd78b
Marlo Longley <mlongley@stanford.edu>
2025-12-04 14:58:54 -0500

    Specify screen width for feedback test

--- spec/features/feedback_form_spec.rb
+++ spec/features/feedback_form_spec.rb
@@ -6,8 +6,11 @@ RSpec.describe 'Feedback Form', :js do
   it 'is visible only after clicking the feedback link' do
     visit root_path
 
-    expect(page).to have_no_css('#feedback')
+    # The feedback link is nested within the collapsible menu for smaller screens
+    # Chrome headless driver was defaulting to 756px width, breaking this test.
+    page.driver.browser.manage.window.resize_to(800, 800)
 
+    expect(page).to have_no_css('#feedback')
     click_on 'Feedback'
     within '#feedback' do
       expect(page).to have_css('form[name="feedback_form"]')
