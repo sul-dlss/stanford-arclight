f99aeeec8858adafd37b41e65d500e108e52e6df
dnoneill <noneill@stanford.edu>
2026-01-30 12:43:45 -0500

    Update component library

--- app/components/search_button_component.rb
+++ app/components/search_button_component.rb
@@ -1,16 +1,10 @@
 # frozen_string_literal: true
 
-# Subclasses Blacklight's SearchButtonComponent so we can set the
-# button text to visually-hidden at all screen sizes
+# This component is used to render the search button in the search bar.
 class SearchButtonComponent < Blacklight::SearchButtonComponent
   def call
-    tag.div(class: 'search-btn-wrapper rounded-end d-flex') do
-      tag.button(class: 'btn btn-primary search-btn', type: 'submit', id: @id) do
-        # Text visible only on md and larger screens
-        tag.span(t('search.search_bar_button'), class: 'd-none d-md-flex') +
-          # Icon visible only on smaller screens
-          tag.span(render(Blacklight::Icons::SearchComponent.new), class: 'd-md-none')
-      end
+    tag.button(class: 'btn btn-primary search-btn', type: 'submit', id: @id) do
+      tag.span(@text, class: 'submit-search-text')
     end
   end
 end
