adad2181bc93af877bb828f65d2faaa21dc50491
Cory Lown <corylown@stanford.edu>
2026-04-24 10:26:04 -0700

    Use Sidekiq in development when environment is set up for it

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
   # Change to :null_store to avoid any caching.
   config.cache_store = :memory_store
 
+  # Use production-like ActiveJob backend (sidekiq) if Redis is available
+  config.active_job.queue_adapter = :sidekiq if ENV.include? 'SIDEKIQ_REDIS_URL'
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
